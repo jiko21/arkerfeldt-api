9ab575dbed3a33e6514325ed9be2f581cee28143
jiko21 <daikikojima0531@icloud.com>
2020-12-13 22:57:06 +0900

    add repository layer for post

---
+++ src/infrastructure/datasource/postRepository.ts
@@ -0,0 +1,37 @@
+import { Post, PostCreateInput } from '@prisma/client';
+import { PostFilterParam } from '../../types/Post';
+import { prismaClient } from './client';
+
+export const findPostById = async (id: number): Promise<Post | null> => {
+  return await prismaClient.post.findOne({
+    where: {
+      id: id,
+    },
+    include: { author: true },
+  });
+};
+
+export const findPosts = async (params: PostFilterParam): Promise<Post[] | null> => {
+  return await prismaClient.post.findMany({
+    where: {
+      AND: [
+        {
+          title: params.title,
+        },
+        {
+          status: params.status,
+        },
+        {
+          isDelete: false,
+        },
+      ],
+    },
+    include: { author: true },
+  });
+};
+
+export const createPost = async (post: PostCreateInput) => {
+  await prismaClient.post.create({
+    data: post,
+  });
+};
---
+++ test/infrastructure/datasource/postRepository.spec.ts
@@ -0,0 +1,247 @@
+import { prismaClient } from '../../../src/infrastructure/datasource/client';
+import {
+  findPostById,
+  findPosts,
+  createPost,
+} from '../../../src/infrastructure/datasource/postRepository';
+import { PublishStatus } from '../../../src/types/Post';
+jest.mock('../../../src/infrastructure/datasource/client');
+
+describe('postRepository.ts', () => {
+  describe('findPostById', () => {
+    let findOneMock: jest.Mock;
+
+    beforeEach(() => {
+      findOneMock = jest.fn();
+      prismaClient.post.findOne = findOneMock;
+    });
+
+    afterEach(() => {
+      findOneMock.mockClear();
+    });
+
+    it('correctly calls when post exists', async () => {
+      const ID = 1;
+      const EXPECTED = {
+        id: ID,
+        title: 'A',
+        content: 'AAA',
+        createdAt: new Date('2020-12-12 12:00:00'),
+        updatedAt: new Date('2020-12-12 12:00:00'),
+        isDelete: false,
+        status: PublishStatus.PUBLISHED,
+        author: {
+          uid: 'aaa',
+          displayName: 'taro',
+          photoUrl: 'aaa',
+        },
+      };
+      findOneMock.mockReturnValueOnce(EXPECTED);
+      const actual = await findPostById(ID);
+      expect(actual).toBe(EXPECTED);
+      expect(findOneMock).toBeCalledWith({
+        where: {
+          id: ID,
+        },
+        include: { author: true },
+      });
+    });
+
+    it('correctly calls when user not exists', async () => {
+      const ID = 1;
+      findOneMock.mockReturnValueOnce(null);
+      const actual = await findPostById(ID);
+      expect(actual).toBe(null);
+      expect(findOneMock).toBeCalledWith({
+        where: {
+          id: ID,
+        },
+        include: { author: true },
+      });
+    });
+
+    it('fails when error occured', async () => {
+      const ID = 1;
+      findOneMock.mockRejectedValue({ msg: 'error' });
+      try {
+        await findPostById(ID);
+      } catch (e) {
+        expect(findOneMock).toBeCalledWith({
+          where: {
+            id: ID,
+          },
+          include: { author: true },
+        });
+      }
+    });
+  });
+
+  describe('findPosts', () => {
+    let findManyMock: jest.Mock;
+
+    beforeEach(() => {
+      findManyMock = jest.fn();
+      prismaClient.post.findMany = findManyMock;
+    });
+
+    afterEach(() => {
+      findManyMock.mockClear();
+    });
+
+    it('correctly calls when post exists', async () => {
+      const params = {
+        title: 'a',
+        status: PublishStatus.PUBLISHED,
+      };
+
+      const EXPECTED = {
+        id: 1,
+        title: 'A',
+        content: 'AAA',
+        createdAt: new Date('2020-12-12 12:00:00'),
+        updatedAt: new Date('2020-12-12 12:00:00'),
+        isDelete: false,
+        status: PublishStatus.PUBLISHED,
+        author: {
+          uid: 'aaa',
+          displayName: 'taro',
+          photoUrl: 'aaa',
+        },
+      };
+      findManyMock.mockReturnValueOnce([EXPECTED]);
+      const actual = await findPosts(params);
+      expect(actual).toEqual([EXPECTED]);
+      expect(findManyMock).toBeCalledWith({
+        where: {
+          AND: [
+            {
+              title: params.title,
+            },
+            {
+              status: params.status,
+            },
+            {
+              isDelete: false,
+            },
+          ],
+        },
+        include: { author: true },
+      });
+    });
+
+    it('correctly calls when user not exists', async () => {
+      const params = {
+        title: 'a',
+        status: PublishStatus.PUBLISHED,
+      };
+      findManyMock.mockReturnValueOnce(null);
+      const actual = await findPosts(params);
+      expect(actual).toBe(null);
+      expect(findManyMock).toBeCalledWith({
+        where: {
+          AND: [
+            {
+              title: params.title,
+            },
+            {
+              status: params.status,
+            },
+            {
+              isDelete: false,
+            },
+          ],
+        },
+        include: { author: true },
+      });
+    });
+
+    it('fails when error occured', async () => {
+      const params = {
+        title: 'a',
+        status: PublishStatus.PUBLISHED,
+      };
+
+      findManyMock.mockRejectedValue({ msg: 'error' });
+      try {
+        await findPosts(params);
+      } catch (e) {
+        expect(findManyMock).toBeCalledWith({
+          where: {
+            AND: [
+              {
+                title: params.title,
+              },
+              {
+                status: params.status,
+              },
+              {
+                isDelete: false,
+              },
+            ],
+          },
+          include: { author: true },
+        });
+      }
+    });
+  });
+
+  describe('createPost', () => {
+    let createPostMock: jest.Mock;
+
+    beforeEach(() => {
+      createPostMock = jest.fn();
+      prismaClient.post.create = createPostMock;
+    });
+
+    afterEach(() => {
+      createPostMock.mockClear();
+    });
+
+    it('correctly calls when post successfully created', async () => {
+      const POST = {
+        id: 1,
+        title: 'A',
+        content: 'AAA',
+        createdAt: new Date('2020-12-12 12:00:00'),
+        updatedAt: new Date('2020-12-12 12:00:00'),
+        isDelete: false,
+        status: PublishStatus.PUBLISHED,
+        author: {
+          connect: {
+            uid: 'aaa',
+          },
+        },
+      };
+      createPostMock.mockReturnValueOnce({});
+      await createPost(POST);
+      expect(createPostMock).toBeCalledWith({
+        data: POST,
+      });
+    });
+
+    it('fails when error occured', async () => {
+      const POST = {
+        id: 1,
+        title: 'A',
+        content: 'AAA',
+        createdAt: new Date('2020-12-12 12:00:00'),
+        updatedAt: new Date('2020-12-12 12:00:00'),
+        isDelete: false,
+        status: PublishStatus.PUBLISHED,
+        author: {
+          connect: {
+            uid: 'aaa',
+          },
+        },
+      };
+      createPostMock.mockRejectedValue({ msg: 'error' });
+      try {
+        await createPost(POST);
+      } catch (e) {
+        expect(createPostMock).toBeCalledWith({
+          data: POST,
+        });
+      }
+    });
+  });
+});
