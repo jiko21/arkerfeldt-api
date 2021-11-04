9047e14ebe87a8a0e511b2c5c94537c4ec825ca5
jiko21 <daikikojima0531@icloud.com>
2021-11-04 21:08:09 +0900

    add: swagger endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express from 'express';
 import { postRouter } from './interfaces/api/router/postRouter';
 import { userRouter } from './interfaces/api/router/userRouter';
+import { serve, setup } from 'swagger-ui-express';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
+import path from 'path';
+import swaggerCombine from 'swagger-combine';
+
 
 export const app = express();
+
+const SWAGGER_PATH = path.join(__dirname, '../swagger/api.yaml');
+// const swaggerDocument = yaml.safeLoad(fs.readFileSync(SWAGGER_PATH, 'utf-8'));
 
 app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/posts', postRouter);
+app.use('/api-docs', serve);
+swaggerCombine(SWAGGER_PATH).then((res) => {
+  app.get('/api-docs', setup(res));
+});
