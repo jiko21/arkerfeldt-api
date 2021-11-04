ab72bbd8423593de63ca81990c2056d3f46eacd4
jiko21 <daikikojima0531@icloud.com>
2021-11-04 21:19:31 +0900

    fix: lint

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,12 @@ import express from 'express';
 import { postRouter } from './interfaces/api/router/postRouter';
 import { userRouter } from './interfaces/api/router/userRouter';
 import { serve, setup } from 'swagger-ui-express';
-import * as yaml from 'js-yaml';
-import * as fs from 'fs';
 import path from 'path';
 import swaggerCombine from 'swagger-combine';
-
 
 export const app = express();
 
 const SWAGGER_PATH = path.join(__dirname, '../swagger/api.yaml');
-// const swaggerDocument = yaml.safeLoad(fs.readFileSync(SWAGGER_PATH, 'utf-8'));
 
 app.use(express.json());
 app.use('/api/user', userRouter);
